b3caa2ed227b0f36d01e34521bdd9afe1ca375c6
Eduard <eduardkarimov.rb@gmail.com>
2023-05-15 01:46:54 +0500

    add new futures

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,18 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
     var count: Int = 0
     var buttonTapPlus: Bool = false
     var buttonTapMinus: Bool = false
     var restartButtonTap: Bool = false
     let textLable: String = "Значение счетчика: "
+    let dateFormatter = DateFormatter()
+    var date: Date = Date()
+    
     @IBOutlet weak var incrementCounter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         incrementCounter.text = textLable + "\(count)"
-        
+        changeHistory.delegate = self
     }
+    
+    func changeHistory(_ textView: UITextView, shouldChangeTextIn _: NSRange, replacementText text: String) -> Bool {
+        let resultRange = text.rangeOfCharacter(from: CharacterSet.newlines, options: .backwards)
+        if text.count == 1 && resultRange != nil {
+            textView.resignFirstResponder()
+            // Do any additional stuff here
+            return false
+        }
+        return true
+    }
+    
+    
     
     @IBAction func counterButtonPlus(_ sender: Any) {
         while buttonTapPlus == true {
@@ -26,6 +41,15 @@ class ViewController: UIViewController {
             count += 1
             print(count)
             incrementCounter.text = textLable + "\(count)"
+        
+        dateFormatter.dateFormat = "MM/dd/yyyy, hh:mm"
+        dateFormatter.timeZone = TimeZone.current
+        let todaysDate = dateFormatter.string(from: date)
+        print(todaysDate)
+        
+        changeHistory.text = "История изменений"
+        
+        changeHistory.insertText("\n" + todaysDate + "," + " " + "Значение:" + " " + "\(count)")
     }
     
     @IBAction func counterButtonMinus(_ sender: Any) {
@@ -47,4 +71,5 @@ class ViewController: UIViewController {
         incrementCounter.text = textLable + "\(count)"
     }
         
-    }
+    @IBOutlet weak var changeHistory: UITextView!
+}
